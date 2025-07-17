614c9a24b81862a1d069dc91cf1b5348b100b68b
cimuletz <72495162+cimuletz@users.noreply.github.com>
2025-07-17 14:40:13 +0300

    added new formulas

--- svd.py
+++ svd.py
@@ -7,6 +7,7 @@ import time
 from time import perf_counter
 import math
 from joblib import Parallel, delayed
+from tqdm import tqdm
 
 np.set_printoptions(suppress=True, precision = 4, linewidth = 200)
 
@@ -31,12 +32,15 @@ class catchtime:
 
 class ApproxSVD():
 
-    def __init__(self, n_iter, p, debug_mode = False):
+    def __init__(self, n_iter, p, score_method = 'cf', debug_mode = False, jobs = -1, stored_g = False):
         self.n_iter = n_iter
         self.p = p
         self.debug_mode = debug_mode
-
+        self.jobs = jobs
         self.logger = logging.getLogger(self.__class__.__name__)
+        self.stored_g = stored_g
+        if self.stored_g:
+            self.g_transforms = []
         if self.debug_mode:
             self.logger.setLevel(logging.DEBUG)
         else:
@@ -47,6 +51,17 @@ class ApproxSVD():
             formatter = logging.Formatter('%(levelname)s - %(message)s')
             handler.setFormatter(formatter)
             self.logger.addHandler(handler)
+        
+        match score_method:
+            case "fro":
+                self.score_fn = self.compute_score_fro
+            case "cf":
+                self.score_fn = self.compute_score_cf
+            case "svd":
+                self.score_fn = self.compute_score_svd
+            case _:
+                raise ValueError("Unknown scoring function")
+
 
 
     # we get a d x N matrix x and a 2x2 matrix m
@@ -78,9 +93,35 @@ class ApproxSVD():
         row_j = x[j, :]
         x[i, :] = m[0][0] * row_i + m[0][1] * row_j
         x[j, :] = m[1][0] * row_i + m[1][1] * row_j
+    
+    def rightMatmulTranspose(self, x, i, j, m):
+        # extract columns i and j
+
+        if i < x.shape[1]:
+            column_i = copy.deepcopy(x[:,i])
+        else:
+            column_i = np.zeros(x.shape[0])
+
+        if j < x.shape[1]:
+            column_j = x[:,j]
+        else:
+            column_j = np.zeros(x.shape[0])
+
+        if i < x.shape[1]:
+            x[:, i] = m[0][0] * column_i + m[0][1] * column_j
+
+        if j < x.shape[1]:    
+            x[:, j] = m[1][0] * column_i + m[1][1] * column_j
 
 
-    def compute_score(self, i, j, x, d):
+    def leftMatmulTranspose(self, x, i, j, m):
+        row_i = copy.deepcopy(x[i, :])
+        row_j = x[j, :]
+        x[i, :] = m[0][0] * row_i + m[1][0] * row_j
+        x[j, :] = m[0][1] * row_i + m[1][1] * row_j
+
+    # score using frobenius norm
+    def compute_score_fro(self, i, j, x, d):
         if j >= d:
             xji = 0
             xjj = 0
@@ -96,9 +137,45 @@ class ApproxSVD():
         c1 = (np.linalg.norm(t, 'fro') ** 2)/2
         c1_squared = c1 ** 2
         c2_squared = np.linalg.det(t) ** 2
-        value = math.sqrt(c1 + math.sqrt(c1_squared - c2_squared)) - t[0][0] # using closed form formula for sigma1
+        value = math.sqrt(c1 + math.sqrt(c1_squared - c2_squared)) - t[0][0]
 
         return (i, j, value)
+    
+    # score using closed form of svd
+    def compute_score_cf(self, i, j, x, d):
+        if j >= d:
+            xji = 0
+            xjj = 0
+        else:
+            xji = x[j][i]
+            xjj = x[j][j]
+
+        if x[i][i] * xjj - x[i][j] * xji >= 0:
+            value = math.sqrt((x[i][i] + xjj)**2 + (x[i][j] - xji)**2) - x[i][i] - xjj
+        else:
+            value = math.sqrt((x[i][i] - xjj)**2 + (x[i][j] + xji)**2) - x[i][i] - xjj
+
+        return (i, j, value)
+
+    # score using explicit svd
+    def compute_score_svd(self, i, j, x , d):
+        if j >= d:
+            xji = 0
+            xjj = 0
+        else:
+            xji = x[j][i]
+            xjj = x[j][j]
+
+        t = np.array([[x[i][i], x[i][j]],
+                      [xji,     xjj]])
+        _, s, _ = np.linalg.svd(t)
+
+        value = s[0] - x[i][i]
+        return (i, j, value)
+    
+    def build_ubar(self, u):
+        for g, i, j in self.g_transforms:
+            self.rightMatmul(u, i, j, g)
 
 
     def fit(self, trueX):
@@ -112,38 +189,18 @@ class ApproxSVD():
         with catchtime(self.debug_mode, self.logger, "total time"):
         
             with catchtime(self.debug_mode, self.logger, "initial scores"):
-            #     results = Parallel(n_jobs=6, prefer="processes")(
-            #         delayed(self.compute_score)(i, j, x, d)
-            #         for i in range(self.p)
-            #         for j in range(i + 1, n)
-            #     )
+                results = Parallel(n_jobs=self.jobs, prefer="processes")(
+                    delayed(self.score_fn)(i, j, x, d)
+                    for i in range(self.p)
+                    for j in range(i + 1, n)
+                )
+                for i, j, value in results:
+                    scores[i][j] = value
 
-            # for i, j, value in results:
-            #     scores[i][j] = value
-            
-                # for i in range(self.p):
-                #     for j in range(i + 1, n):
-                #         scores[i][j] = self.compute_score(i,j,x,d)[2]
-
-                for i in range(self.p):
-                    for j in range(i + 1, n):
-                        if j >= d:
-                            xji = 0
-                            xjj = 0 
-                        else:
-                            xji = x[j][i]
-                            xjj = x[j][j]
-
-                        t = np.array([[x[i][i], x[i][j]],
-                                    [xji, xjj]])
-                        _, s, _ = np.linalg.svd(t)
-
-                        scores[i][j] = s[0] - x[i][i]
-
-            for q in range(self.n_iter):
+            for q in tqdm(range(self.n_iter)):
                 # get max score from matrix
-                with catchtime(self.debug_mode, self.logger, "find max score"):
-                    iq, jq = np.unravel_index(np.argmax(scores), scores.shape)
+                #with catchtime(self.debug_mode, self.logger, "find max score"):
+                iq, jq = np.unravel_index(np.argmax(scores), scores.shape)
                 
                 if jq >= d:
                     xji = 0
@@ -151,91 +208,41 @@ class ApproxSVD():
                 else:
                     xji = x[jq][iq]
                     xjj = x[jq][jq]
-                with catchtime(self.debug_mode, self.logger, "perform matrix mul"):
-                    t = np.array([
-                                    [x[iq][iq], x[iq][jq]], 
-                                    [xji,       xjj]
-                                ])
-                    G, _, H = np.linalg.svd(t)
+                #with catchtime(self.debug_mode, self.logger, "perform matrix mul"):
+                t = np.array([
+                                [x[iq][iq], x[iq][jq]], 
+                                [xji,       xjj]
+                            ])
+                G, _, H = np.linalg.svd(t)
 
-                    # update intermediate x and u
-                    self.rightMatmul(x, iq, jq, H.transpose()) # equivalent to x @ H.transpose()
+                # update intermediate x and u
+                self.rightMatmulTranspose(x, iq, jq, H) # equivalent to x @ H.transpose()
+                if self.stored_g == False:
                     self.rightMatmul(u, iq, jq, G) # equivalent to u @ G
-                    
-                    if jq < d:
-                        self.leftMatmul(x, iq, jq, G.transpose()) # equivalent to G.transpose() @ x
+                else:
+                    self.g_transforms.append((G, iq, jq))
                 
-                with catchtime(self.debug_mode, self.logger, "update scores"):
-                    # update scores
-                    for s in range(iq + 1, d):
-                        t = np.array([
-                                        [x[iq][iq], x[iq][s]],
-                                        [x[s][iq], x[s][s]]
-                                    ])
-                        _, sig, _ = np.linalg.svd(t)
-                        scores[iq][s] = sig[0] - x[iq][iq]
-                        # if x[iq][iq] * x[s][s] - x[iq][s] *x[s][iq] >= 0:
-                        #     scores[iq][s] = math.sqrt((x[iq][iq] + x[s][s])**2 + (x[iq][s] - x[s][iq])**2) - x[iq][iq] - x[s][s]
-                        # else:
-                        #     scores[iq][s] = math.sqrt((x[iq][iq] - x[s][s])**2 + (x[iq][s] + x[s][iq])**2) - x[iq][iq] - x[s][s]
-                        #scores[iq][s] = self.compute_score(iq, s, x, d)[2]
+                if jq < d:
+                    self.leftMatmulTranspose(x, iq, jq, G) # equivalent to G.transpose() @ x
+                
+                #with catchtime(self.debug_mode, self.logger, "update scores"):
+                # update scores
+                for s in range(iq + 1, d):
+                    scores[iq][s] = self.score_fn(iq, s, x, d)[2]
 
-
-                    if jq < self.p:
-                        for s in range(jq + 1, n):
-                            if s >= d:
-                                xsjq = 0
-                                xss = 0
-                            else:
-                                xsjq = x[s][jq]
-                                xss = x[s][s]
-
-                            t = np.array([
-                                            [x[jq][jq], x[jq][s]],
-                                            [xsjq, xss]
-                                        ])
-                            _, sig, _ = np.linalg.svd(t)
-                            scores[jq][s] = sig[0] - x[jq][jq]
-                            # if x[jq][jq] * xss - x[jq][s] *xsjq >= 0:
-                            #     scores[jq][s] = math.sqrt((x[jq][jq] + xss)**2 + (x[jq][s] - xsjq)**2) - x[jq][jq] - xss
-                            # else:
-                            #     scores[jq][s] = math.sqrt((x[jq][jq] - xss)**2 + (x[jq][s] + xsjq)**2) - x[jq][jq] - xss
-                            #scores[iq][s] = self.compute_score(jq, s, x, d)[2]
-                        
-                    for r in range(iq):
-                        t = np.array([[x[r][r], x[r][iq]],
-                                    [x[iq][r], x[iq][iq]]])
-                        _, sig, _ = np.linalg.svd(t)
-                        scores[r][iq] = sig[0] - x[r][r]
-                        # if x[r][iq] * x[iq][iq] - x[r][iq] *x[iq][r] >= 0:
-                        #     scores[r][iq] = math.sqrt((x[r][r] + x[iq][iq])**2 + (x[r][iq] - x[iq][r])**2) - x[r][r] - x[iq][iq]
-                        # else:
-                        #     scores[r][iq] = math.sqrt((x[r][r] - x[iq][iq])**2 + (x[r][iq] + x[iq][r])**2) - x[r][r] - x[iq][iq]
-                        #scores[r][iq] = self.compute_score(r, iq, x, d)[2]
+                if jq < self.p:
+                    for s in range(jq + 1, n):
+                        scores[jq][s] = self.score_fn(jq, s, x, d)[2]
                     
-                    for r in range(min(jq, self.p)):
-                        if jq >= d:
-                            x_jq_r = 0
-                            x_jq_jq = 0
-                        else:
-                            x_jq_r = x[jq][r]
-                            x_jq_jq = x[jq][jq]
-
-                        t = np.array([
-                                        [x[r][r], x[r][jq]],
-                                        [x_jq_r, x_jq_jq]
-                                    ])
-                        _, sig, _ = np.linalg.svd(t)
-
-                        scores[r][jq] = sig[0] - x[r][r]
-                        # if x[r][jq] * x_jq_jq - x[r][jq] * x_jq_r >= 0:
-                        #     scores[r][jq] = math.sqrt((x[r][r] + x_jq_jq)**2 + (x[r][jq] - x_jq_r)**2) - x[r][r] - x_jq_jq
-                        # else:
-                        #     scores[r][jq] = math.sqrt((x[r][r] - x_jq_jq)**2 + (x[r][jq] + x_jq_r)**2) - x[r][r] - x_jq_jq
-                        #scores[r][jq] = self.compute_score(r, jq, x, d)[2]
+                for r in range(iq):
+                    scores[r][iq] = self.score_fn(r, iq, x, d)[2]
+                
+                for r in range(min(jq, self.p)):
+                    scores[r][jq] = self.score_fn(r, jq, x, d)[2]
                     
                 traces = np.append(traces,  np.trace(x[:self.p, :self.p]))
-        
+            if self.stored_g == True:
+                self.build_ubar(u)
         return traces, u, x
 
         
--- tests.py
+++ tests.py
@@ -5,14 +5,14 @@ from scipy.sparse.linalg import svds
 
 np.random.seed(42)
 
-def run_tests(num_tests=10):
+def run_tests(num_tests=10, score_fn = 'svd'):
     for i in range(num_tests):
         random_matrix = np.random.rand(6, 20)
         
         u, s, vt = np.linalg.svd(random_matrix)
 
         p = 4
-        approx_svd = ApproxSVD(200, p, False)
+        approx_svd = ApproxSVD(200, p, score_fn, False)
         traces, ubar, x_approx = approx_svd.fit(random_matrix)
 
         aux = u.transpose() @ random_matrix @ vt.transpose()
